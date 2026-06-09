3a914b7b3252685a44fee368e64503ab6806bf34
Razqa <razqaazaki55@gmail.com>
2026-06-09 14:44:40 +0700

    modul12

---
+++ PRETEST/src/Anggota.java
@@ -0,0 +1,12 @@
+public class Anggota {
+    private String nama;
+    private String nomorAnggota;
+
+    public Anggota(String nama, String nomorAnggota) {
+        this.nama = nama;
+        this.nomorAnggota = nomorAnggota;
+    }
+
+    public String getNama() { return nama; }
+    public String getNomorAnggota() { return nomorAnggota; }
+}
